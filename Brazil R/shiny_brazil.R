02422743c7e36fb0d233841d1ae9e924552635bb
Kate Colvin <103461663+kaaateco@users.noreply.github.com>
2022-07-07 16:53:52 -0400

    Add files via upload

--- Brazil R/shiny_brazil.R
+++ Brazil R/shiny_brazil.R
@@ -1,12 +1,18 @@
 library(shiny)
+library(dplyr)
 
+source("brazil_data_read.R")
 
+server <- plot_ly(brazil, x = ~date, y =~I, type = "bar", hoverinfo = "text") %>%
+  layout(barmode = "stack", title = list(xanchor = "left", x = 0), legend =
+           list(orientation = "h", font = list(size = 16))) %>%
+  plotly::config(toImageButtonOptions = list(width = NULL, height = NULL))
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("COVID-19 in Brazil 4/1/20-6/1/20"),
 
   imageOutput(
     outputId,
@@ -23,24 +29,24 @@ ui <- fluidPage(
 #     ),
 #
 #     # Show a plot of the generated distribution
-#     mainPanel(
-#       plotOutput("distPlot")
-#     )
-#   )
+     mainPanel(
+       plotOutput("distPlot")
+     )
+   )
 # )
 #
 # # Define server logic required to draw a histogram
-# server <- function(input, output) {
-#
-#   output$distPlot <- renderPlot({
-#     # generate bins based on input$bins from ui.R
-#     x    <- faithful[, 2]
-#     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-#
-#     # draw the histogram with the specified number of bins
-#     hist(x, breaks = bins, col = 'darkblue', border = 'white')
-#   })
-# }
+server <- function(input, output) {
+
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkblue', border = 'white')
+  })
+ }
 
 # Run the application
 shinyApp(ui = ui, server = server)
